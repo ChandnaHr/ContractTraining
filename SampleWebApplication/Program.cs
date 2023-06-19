5539f4eb1232fc78ea26201872fe4b1c1774f769
Chandana H R <Chandana.HR@qualitestgroup.com>
2023-06-19 21:17:13 +0530

    Add project files.

---
+++ SampleWebApplication/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore;
+using SampleWebApplication.Services;
+
+namespace SampleWebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddControllers();
+                    services.AddScoped<IUserService, UserService>();
+                    services.AddSwaggerGen();
+
+                })
+                .Configure((hostContext, app) =>
+                {
+                    if (hostContext.HostingEnvironment.IsDevelopment())
+                    {
+                        app.UseDeveloperExceptionPage();
+                    }
+                    app.UseStaticFiles();
+                    app.UseRouting();
+                    app.UseEndpoints(endpoints =>
+                    {
+                        endpoints.MapControllers();
+                    });
+
+                    app.UseSwagger();
+                    app.UseSwaggerUI(c => {
+                        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V2");
+                        
+                    });
+                    app.UseHttpsRedirection();
+
+                    
+                });
+
+    }
+
+
+
+}
---
+++ SampleWebApplication/Services/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using SampleWebApplication.Models;
+
+namespace SampleWebApplication.Services
+{
+    public interface IUserService
+    {
+        Task<UserDetails> GetUserDetails(int id);
+        Task<CreateUserResponse> CreateUser(CreateUserRequest createUserRequest);
+        Task<RegisterUserResponse> RegisterUser(RegisterUserRequest registerUserRequest);
+    }
+}
